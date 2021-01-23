db7d3587e599490fafba5039d63f8fa368d02ceb
Santi-1997 <Santiagoortigoza@hotmail.com>
2021-01-23 16:59:45 -0300

    Agregando archivos html y css

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ app.get ('/', function(req, res) {
     res.sendFile(path.resolve('./views/index.html'));
 });
 
+app.get ('/Producto', function(req, res) {
+    res.sendFile(path.resolve('./views/productDetail.html'));
+});
+
+app.get ('/Carrito', function(req, res) {
+    res.sendFile(path.resolve('./views/productCart.html'));
+});
+
+app.get ('/Registro', function(req, res) {
+    res.sendFile(path.resolve('./views/register.html'));
+});
+
+app.get ('/Login', function(req, res) {
+    res.sendFile(path.resolve('./views/login.html'));
+});
+
 app.listen(3000, function(){
     console.log('Servidor Corriendo en el puerto 3000')
 });
