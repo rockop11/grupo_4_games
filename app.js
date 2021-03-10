249d0dedb2969a242ff4ca887634791f4f69f238
rockop11 <rockoponce1422@gmail.com>
2021-03-10 16:19:53 -0300

    seguimos ruteando

--- app.js
+++ app.js
@@ -27,8 +27,26 @@ app.use('/products', productsRoutes);
 //Rutas usuarios
 app.use('/users', usersRoutes);
 
-
 //Servidor
 app.listen(3000, function(){
     console.log('Servidor Corriendo en el puerto 3000')
 });
+
+// ** DON'T TOUCH FROM HERE **
+// ** catch 404 and forward to error handler **
+app.use((req, res, next) => next(createError(404)));
+
+// ** error handler **
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.path = req.path;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error',{titulo:"error 404"});
+});
+
+// ** exports app - dont'touch **
+module.exports = app;
--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const { Recoverable } = require('repl');
 
 
 const productsController = {
@@ -21,19 +22,6 @@ const productsController = {
 
         let productDetail = productosJSON.find(productDetail=>productDetail.id==req.params.id)
         res.render('products/productDetail',{productDetail})
-
-        //ANDA X LA MITAD
-        // for (let i=0 ; i<productosJSON.length ; i ++){
-        //     if(productosJSON[i].id == req.params.id){
-        //         return res.render('products/productDetail', {productosJSON});
-        //     } else {
-
-        //     };  
-        // };
-        //let product = productosJSON.find(function(product){
-            //product.id == req.params.id
-        //})
-        //res.render('products/productDetail', {productosJSON});
     },
 
     create: (req, res) => {
@@ -46,9 +34,11 @@ const productsController = {
         let producto = fs.readFileSync(productos, 'utf-8');
         //MANIPULO EL JSON Y LO CONVIERTO EN OBJ. LIT.
         let productosJSON = JSON.parse(producto);
+        let ids = productosJSON.map(p=>p.id); //guardar en un array todos los ids
+        let id= Math.max(...ids)+1; //filtra el mayor de los ids del array, se le suma 1 para el id del nuevo producto
         //Valores Nuevos de mi Obj Literal.
         productosJSON.push({
-            id: req.body.id,
+            id: id,
             nombre: req.body.nombre,
             descripcion: req.body.descripcion,
             imagen: req.files[0].filename,
@@ -57,14 +47,12 @@ const productsController = {
             interes: req.body.interes
         })  
         
+        
         fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(productosJSON, null, ' '));
-        res.redirect('/products')
-        
-        //res.redirect('/products')
-
-        
+        res.redirect('/products')  
     },
 
+    //VISTA DE EDITAR PRODUCTO
     edit: (req,res) => {
         let productos = path.join(__dirname, '../data/products.json');
         let producto = fs.readFileSync(productos, 'utf-8');
@@ -72,8 +60,32 @@ const productsController = {
         res.render('products/productEdit', {productosJSON});
     }, 
 
+    //EDITAR PRODUCTO POR POST!
     update: (req,res) => {
-    //     res.send('edicion del producto');
+        let productos = path.join(__dirname, '../data/products.json');
+        let producto = fs.readFileSync(productos, 'utf-8');
+        let productosJSON = JSON.parse(producto);
+        let id = req.params.id
+        let productoAEditar = productosJSON.find(p=>p.id == id)
+        let imagen
+        if( req.file !=undefined){
+            imagen = req.file.filename
+        } else {
+            imagen = productoAEditar.imagen
+        }
+        productoAEditar = {
+            id: productoAEditar.id,
+            ...req.body,
+            imagen: imagen
+        }
+        let nuevoProducto = productosJSON.map(p => {
+            if(p.id == productoAEditar.id){
+                return p = {...productoAEditar}  
+            }
+            return p
+        })
+        fs.writeFileSync(productos, JSON.stringify(nuevoProducto, null, ' '));
+        res.redirect('/products');
     }, 
 
     delete: (req,res) => {
