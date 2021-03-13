0d3ea3f70a2885a04d4aa443ec8eaf536b74d06b
Santi-1997 <Santiagoortigoza@hotmail.com>
2021-03-13 20:55:49 -0300

    agregando el search

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,8 +1,23 @@
 const path = require('path');
+const fs = require('fs');
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const mainController = {
     index: function(req, res) {
         res.render('index');
+    },
+    search: (req,res)=>{
+        let productos = path.join(__dirname, '../data/products.json');
+        let producto = fs.readFileSync(productos, 'utf-8');
+        let productosJSON = JSON.parse(producto);
+        let search = req.query.keywords;
+        let productsToSearch = productosJSON.filter(p=> p.nombre.toLowerCase().includes(search));
+        res.render('products/result', {
+            products: productsToSearch, 
+            search,
+            toThousand,
+        })
     }
 }
 
