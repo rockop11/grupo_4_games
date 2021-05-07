fe5a517c0f7d130e5e533fdfa5fd0ae8bace8401
rockop11 <rockoponce1422@gmail.com>
2021-05-07 15:08:36 -0300

    estilizando el duos

--- public/js/menuDuos.js
+++ public/js/menuDuos.js
@@ -5,17 +5,21 @@ window.onload = function () {
 
     ps.addEventListener('mouseover', function (){
         ps.style.width = "400%";
+        ps.style.transition = "600ms"
     })
 
     ps.addEventListener('mouseleave', function(){
         ps.style.width = "50%"
+        ps.style.transition = "600ms"
     })
 
     xb.addEventListener('mouseover', function (){
         xb.style.width = "400%"
+        xb.style.transition = "600ms"
     })
 
     xb.addEventListener('mouseleave', function(){
         xb.style.width = "50%"
+        xb.style.transition = "600ms"
     })
 }
