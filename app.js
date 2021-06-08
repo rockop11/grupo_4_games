94a37b84ef86369c4d2c49ee80f066c1fb7fa851
molinajuan <molina.juanignacio@gmail.com>
2021-06-08 19:15:17 -0300

    página de error 404

--- app.js
+++ app.js
@@ -62,10 +62,27 @@ app.use('/users', usersRoutes);
 app.use('/api/products', productsApiRouter);
 app.use('/api/users', usersApiRouter);
 
-
 let port = process.env.PORT || 3000;
 //Servidor
 app.listen(port, function(){
     console.log(`El servidor está corriendo en el puerto ${port}`)
 });
 
+// ** DON'T TOUCH FROM HERE **
+// ** catch 404 and forward to error handler **
+
+app.use((req, res, next) => next(createError(404)));
+
+//Error handler
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.path = req.path;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  module.exports = app;
